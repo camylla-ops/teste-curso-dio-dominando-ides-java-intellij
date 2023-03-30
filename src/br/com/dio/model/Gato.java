c5d02944d569a3bbc65ca3d5e46434e364966be6
camylla <camylla.oliveiraps@gmail.com>
2023-03-30 20:04:33 -0300

    firt commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,62 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private String idade;
+
+    public Gato (){}
+    public Gato(String nome, String cor, String idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade='" + idade + '\'' +
+                '}';
+    }
+}
